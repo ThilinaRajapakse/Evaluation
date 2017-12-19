a45cd7f9cc7262d6f9ca8df79cc8bd213e2ce950
Thilina Rajapakse <chaturangarajapakshe@gmail.com>
2017-12-19 15:36:29 +0530

    wip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from sort_contours import sort_contours
 
 img = cv2.imread('sample.jpg')
 # img = cv2.resize(img, None, fx=0.2, fy=0.2)
@@ -39,32 +40,33 @@ auto = auto_canny(blurred)
 edges = cv2.Canny(imgray,100,200)
 
 image, contours, hierarchy = cv2.findContours(blurred,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+contours, bounding_boxes = sort_contours(contours)
 
 # img = cv2.drawContours(img, contours, -1, (0,255,0), 3)
 
 # plt.imshow(img, cmap='gray')
 # plt.show()
 boxes = []
+contour_boxes = []
 
 for cont in contours:
-    if cv2.contourArea(cont) > 500:
+    if cv2.contourArea(cont) > 700:
         rect = cv2.minAreaRect(cont)
         points = cv2.boxPoints(rect)
         points = np.int0(points)
 
         if 0.7 < (points[0][1] - points[1][1]) / (points[2][0] - points[0][0]) < 0.9:
             boxes.append(points)
+            contour_boxes.append(cont)
 
 options = []
 for box in boxes:
     if box[0][0] > 1000:
-        options.append(box)
+        if box[0][0] == box[1][0] and box[3][0] == box[2][0] and box[0][1] == box[3][1] and box[1][1] == box[2][1]:
+            options.append(box)
 
 boxes = options
-img = cv2.drawContours(img, boxes, -1, (0,255,0), 3)
-
-print(len(boxes))
-print(boxes[0].shape)
+img = cv2.drawContours(img, boxes, -1, (0,255,0), 1)
 
 coords = np.concatenate(boxes)
 xmax, ymax = np.max(coords, axis=0)
@@ -72,9 +74,12 @@ xmin, ymin = np.min(coords, axis=0)
 
 img = img[ymin:ymax, xmin:xmax]
 
-print(xmax, xmin, ymax, ymin)
+print(len(boxes))
 
 plt.imshow(img, cmap='gray')
 plt.show()
+
+def get_range
+
 
 
